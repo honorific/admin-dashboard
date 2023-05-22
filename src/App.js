5dfa6283d1ed2709f1b4418646645dbb2594c01b
honorific <honorific.info@gmail.com>
2023-05-22 09:52:32 +0330

    add file ofpages and install reacr-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from './pages/home/Home'
+
 const App = () => {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  )
 }
 
 export default App
