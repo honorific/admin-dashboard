5bb62cde4d2c76e968ff1938102c614cbfd93e92
honorific <honorific.info@gmail.com>
2023-05-23 10:54:29 +0330

    add class name of icom to icons

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -23,55 +23,55 @@ const Sidebar = () => {
         <ul>
           <p className='title'>MAIN</p>
           <li>
-            <DashboardIcon />
+            <DashboardIcon className='icon' />
             <span>Dashboard</span>
           </li>
           <p className='title'>LISTS</p>
           <li>
-            <PersonOutlineOutlinedIcon />
+            <PersonOutlineOutlinedIcon className='icon' />
             <span>Users</span>
           </li>
           <li>
-            <StoreIcon />
+            <StoreIcon className='icon' />
             <span>Products</span>
           </li>
           <li>
-            <PaymentIcon />
+            <PaymentIcon className='icon' />
             <span>Orders</span>
           </li>
           <li>
-            <LocalShippingIcon />
+            <LocalShippingIcon className='icon' />
             <span>Delivery</span>
           </li>
           <p className='title'>USEFUL</p>
           <li>
-            <InsertChartIcon />
+            <InsertChartIcon className='icon' />
             <span>Stats</span>
           </li>
           <li>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon className='icon' />
             <span>Notifications</span>
           </li>
           <p className='title'>SERVICE</p>
           <li>
-            <SettingsSystemDaydreamOutlinedIcon />
+            <SettingsSystemDaydreamOutlinedIcon className='icon' />
             <span>System Health</span>
           </li>
           <li>
-            <PsychologyOutlinedIcon />
+            <PsychologyOutlinedIcon className='icon' />
             <span>Logs</span>
           </li>
           <li>
-            <SettingsIcon />
+            <SettingsIcon className='icon' />
             <span>Settings</span>
           </li>
           <p className='title'>USER</p>
           <li>
-            <AccountCircleOutlinedIcon />
+            <AccountCircleOutlinedIcon className='icon' />
             <span>Profile</span>
           </li>
           <li>
-            <LogoutOutlinedIcon />
+            <LogoutOutlinedIcon className='icon' />
             <span>Logout</span>
           </li>
         </ul>
