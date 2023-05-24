b01ff40edfeaf4cb16d58ee4c6add2106c7f2547
honorific <honorific.info@gmail.com>
2023-05-24 12:03:21 +0330

    styling the widget

--- src/components/widget/Widget.js
+++ src/components/widget/Widget.js
@@ -1,10 +1,21 @@
 import './widget.scss'
-
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
 const Widget = () => {
   return (
     <div className='widget'>
-      <div className='left'>left</div>
-      <div className='right'>right</div>
+      <div className='left'>
+        <span className='title'>USERS</span>
+        <span className='counter'>1244</span>
+        <span className='link'>See all users</span>
+      </div>
+      <div className='right'>
+        <div className='percentage positive'>
+          <KeyboardArrowUpIcon />
+          20%
+        </div>
+        <PersonOutlinedIcon className='icon' />
+      </div>
     </div>
   )
 }
