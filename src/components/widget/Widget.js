6ee67652cb955910a3977fb6cc41f1764a269fb0
honorific <honorific.info@gmail.com>
2023-05-25 21:46:46 +0330

    switch case for different type prop of widget

--- src/components/widget/Widget.js
+++ src/components/widget/Widget.js
@@ -1,7 +1,49 @@
 import './widget.scss'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
-const Widget = () => {
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined'
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined'
+
+const Widget = ({type}) => {
+  let data
+
+  switch (type) {
+    case 'user':
+      data = {
+        title: 'USERS',
+        isMoney: false,
+        link: 'see all users',
+        icon: <PersonOutlinedIcon className='icon' />,
+      }
+      break
+    case 'order':
+      data = {
+        title: 'ORDERS',
+        isMoney: true,
+        link: 'View net earnings',
+        icon: <ShoppingCartOutlinedIcon className='icon' />,
+      }
+      break
+
+    case 'earning':
+      data = {
+        title: 'EARNING',
+        isMoney: false,
+        link: 'View all orders',
+        icon: <MonetizationOnOutlinedIcon className='icon' />,
+      }
+      break
+    case 'balance':
+      data = {
+        title: 'BALANCE',
+        isMoney: true,
+        link: 'See details',
+        icon: <AccountBalanceWalletOutlinedIcon className='icon' />,
+      }
+      break
+  }
+
   return (
     <div className='widget'>
       <div className='left'>
