4330728c31664d03bf6dc90dff3ffd8e4768f7b3
honorific <honorific.info@gmail.com>
2023-05-26 12:12:37 +0330

    featured and chart layout

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,16 @@
+import './featured.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className='top'>
+        <h1 className='title'>Total Revenue</h1>
+        <MoreVertIcon fontSize='small' />
+      </div>
+      <div className='bottom'></div>
+    </div>
+  )
+}
+
+export default Featured
