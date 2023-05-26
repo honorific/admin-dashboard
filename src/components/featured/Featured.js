e36b45cb2ba2c5bfc892280a0dc144d283505353
honorific <honorific.info@gmail.com>
2023-05-26 13:19:16 +0330

    install rechart and complete styling of amounts

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -2,6 +2,8 @@ import './featured.scss'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import {CircularProgressbar} from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 const Featured = () => {
   return (
@@ -19,7 +21,32 @@ const Featured = () => {
         <p className='desc'>
           previous transaction processing. Last payment may not be included
         </p>
-        <div className='summary'></div>
+
+        <div className='summary'>
+          <div className='item'>
+            <div className='itemTitle'>Target</div>
+            <div className='itemResult negative'>
+              <KeyboardArrowDownIcon fontSize='small' />
+              <div className='resultAmount'>$12.4K</div>
+            </div>
+          </div>
+
+          <div className='item'>
+            <div className='itemTitle'>Last Week</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpIcon fontSize='small' />
+              <div className='resultAmount'>$12.4K</div>
+            </div>
+          </div>
+
+          <div className='item'>
+            <div className='itemTitle'>Last Month</div>
+            <div className='itemResult positive'>
+              <KeyboardArrowUpIcon fontSize='small' />
+              <div className='resultAmount'>$12.4K</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
