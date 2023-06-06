f427487cd1012d9da8d138d70e5f7597c2d58067
honorific <honorific.info@gmail.com>
2023-06-06 12:35:35 +0330

    create skeleton of single page

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -1,7 +1,30 @@
 import './single.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 
 const Single = () => {
-  return <div>Single</div>
+  return (
+    <div className='single'>
+      <Sidebar />
+      <div className='singleContainer'>
+        <Navbar />
+        <div className='top'>
+          <div className='left'>
+            <h1 className='title'>Information</h1>
+            {/* <div className='item'>
+              <img
+                src='https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
+                alt=''
+                className='itemImg'
+              />
+            </div> */}
+          </div>
+          <div className='right'></div>
+        </div>
+        <div className='bottom'></div>
+      </div>
+    </div>
+  )
 }
 
 export default Single
