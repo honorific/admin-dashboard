34c2b0daef3122d16fdb06b566bded53a6c2a640
honorific <honorific.info@gmail.com>
2023-06-07 11:30:33 +0330

    user empty profile

--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -12,7 +12,12 @@ const New = () => {
           <h1>Add new user</h1>
         </div>
         <div className='bottom'>
-          <div className='left'>left</div>
+          <div className='left'>
+            <img
+              src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              alt=''
+            />
+          </div>
           <div className='right'>right</div>
         </div>
       </div>
