e6e315cd136540b7f6e6d014041b1462af4d4cb0
honorific <honorific.info@gmail.com>
2023-06-07 11:19:42 +0330

    new user page skeleton

--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -1,7 +1,23 @@
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './new.scss'
 
 const New = () => {
-  return <div>New</div>
+  return (
+    <div className='new'>
+      <Sidebar />
+      <div className='newContainer'>
+        <Navbar />
+        <div className='top'>
+          <h1>Add new user</h1>
+        </div>
+        <div className='bottom'>
+          <div className='left'>left</div>
+          <div className='right'>right</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default New
