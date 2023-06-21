8f6b59d9999c0f5475aadac9916593b6c7564bc1
honorific <honorific.info@gmail.com>
2023-06-21 23:25:27 +0330

    dark mode file import and initialize

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Single from './pages/single/Single'
 import New from './pages/new/New'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {productInputs, userInputs} from './formSource'
-
+import './style/dark.scss'
 const App = () => {
   return (
-    <div className='App'>
+    <div className='app dark'>
       <BrowserRouter>
         <Routes>
           <Route path='/'>
