6980ea9a59da136b296a177b3989ca6c2f50c692
honorific <honorific.info@gmail.com>
2023-06-30 18:45:20 +0330

    implement toggle dark mode onClick

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,8 +7,11 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
 import ListOutlinedIcon from '@mui/icons-material/ListOutlined'
 import img from '../../img/images.jpeg'
+import {useContext} from 'react'
+import {DarkModeContext} from '../../context/darkModeContext'
 
 const Navbar = () => {
+  const {dispatch} = useContext(DarkModeContext)
   return (
     <div className='navbar'>
       <div className='wrapper'>
@@ -23,7 +26,10 @@ const Navbar = () => {
             English
           </div>
           <div className='item'>
-            <DarkModeOutlinedIcon className='icon' />
+            <DarkModeOutlinedIcon
+              className='icon'
+              onClick={() => dispatch({type: 'TOGGLE'})}
+            />
           </div>
           <div className='item'>
             <FullscreenExitOutlinedIcon className='icon' />
